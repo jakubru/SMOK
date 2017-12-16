a5c74d8be1fb335aad64fc06e9a60884fd24406c
Kuba <jakubruszil@gmail.com>
2017-12-16 20:09:28 +0100

    Check commit

--- app/src/main/java/test/smok/logic/GSMDataCollector.java
+++ app/src/main/java/test/smok/logic/GSMDataCollector.java
@@ -1,22 +1,50 @@
 package test.smok.logic;
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.telephony.CellIdentityGsm;
+import android.telephony.CellInfo;
 import android.telephony.CellInfoGsm;
 import android.telephony.CellSignalStrengthGsm;
+import android.telephony.TelephonyManager;
+
+
+import java.lang.annotation.Target;
+import java.util.List;
+
 
 /**
  * Created by Kuba on 16.12.2017.
  */
 @TargetApi(17)
 public class GSMDataCollector implements DataCollector {
+    private TelephonyManager mTelephonyManager;
 
-    private CellInfoGsm cig;
+    public GSMDataCollector()
+    {
 
+    }
 
     @Override
-    public String collect() {
-        CellIdentityGsm cellIdentityGsm = cig.getCellIdentity();
-        CellSignalStrengthGsm cellSignalStrengthGsm = cig.getCellSignalStrength();
-        return cellIdentityGsm.getCid() + "";
+    public String collect(Context context) {
+        List<CellInfo> cellInfoList = null;
+        String returnString = "bład";
+        this.mTelephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        try{
+            cellInfoList = mTelephonyManager.getAllCellInfo();
+        }
+        catch(SecurityException e){
+
+        }
+        for (CellInfo cellInfo:cellInfoList)
+        {
+            if(cellInfo instanceof CellInfoGsm)
+            {
+                CellInfoGsm cellInfoGsm = (CellInfoGsm) cellInfo;
+                CellIdentityGsm cellIdentityGsm = cellInfoGsm.getCellIdentity();
+                CellSignalStrengthGsm cellSignalStrengthGsm = cellInfoGsm.getCellSignalStrength();
+                returnString = "CID:";
+            }
+        }
+        return returnString;
     }
 }
