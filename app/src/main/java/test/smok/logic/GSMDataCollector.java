55334807c0ceaebce05ec231ee61083a16286397
Kuba <jakubruszil@gmail.com>
2017-12-16 20:45:42 +0100

    Dodany wzorzec mostu i metody służące do zbierania informacji z BTS-ów.

--- app/src/main/java/test/smok/logic/GSMDataCollector.java
+++ app/src/main/java/test/smok/logic/GSMDataCollector.java
@@ -2,9 +2,12 @@ package test.smok.logic;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.telephony.CellIdentityGsm;
+import android.telephony.CellIdentityWcdma;
 import android.telephony.CellInfo;
 import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoWcdma;
 import android.telephony.CellSignalStrengthGsm;
+import android.telephony.CellSignalStrengthWcdma;
 import android.telephony.TelephonyManager;
 
 
@@ -15,7 +18,7 @@ import java.util.List;
 /**
  * Created by Kuba on 16.12.2017.
  */
-@TargetApi(17)
+@TargetApi(26)
 public class GSMDataCollector implements DataCollector {
     private TelephonyManager mTelephonyManager;
 
@@ -42,8 +45,19 @@ public class GSMDataCollector implements DataCollector {
                 CellInfoGsm cellInfoGsm = (CellInfoGsm) cellInfo;
                 CellIdentityGsm cellIdentityGsm = cellInfoGsm.getCellIdentity();
                 CellSignalStrengthGsm cellSignalStrengthGsm = cellInfoGsm.getCellSignalStrength();
-                returnString = "CID:";
+                returnString = "CID:" + cellIdentityGsm.getCid() + ";LAC:" + cellIdentityGsm.getLac() + ";ARFCN:" + cellIdentityGsm.getArfcn() + ";MCC:" + cellIdentityGsm.getMcc()
+                + ";BSIC:" + cellIdentityGsm.getBsic() + ";MNC:" + cellIdentityGsm.getMnc() + ";PSC:" + cellIdentityGsm.getPsc() + ";AsuLevel:" + cellSignalStrengthGsm.getAsuLevel() +
+                ";DBM" + cellSignalStrengthGsm.getDbm() + ";Level:" + cellSignalStrengthGsm.getLevel() + ";TimingAdvance:" + cellSignalStrengthGsm.getTimingAdvance();
             }
+            if(cellInfo instanceof CellInfoWcdma){
+                CellInfoWcdma cellInfoWcdma = (CellInfoWcdma) cellInfo;
+                CellIdentityWcdma cellIdentityWcdma = cellInfoWcdma.getCellIdentity();
+                CellSignalStrengthWcdma cellSignalStrengthWcdma = cellInfoWcdma.getCellSignalStrength();
+                returnString = "CID:" + cellIdentityWcdma.getCid();/* + ";LAC:" + cellIdentityWcdma.getLac() + ";ARFCN:" + cellIdentityWcdma.getUarfcn() + ";MCC:" + cellIdentityWcdma.getMcc()
+                        + ";MNC:" + cellIdentityWcdma.getMnc() + ";PSC:" + cellIdentityWcdma.getPsc() + ";AsuLevel:" + cellSignalStrengthWcdma.getAsuLevel() +
+                        ";DBM" + cellSignalStrengthWcdma.getDbm() + ";Level:" + cellSignalStrengthWcdma.getLevel();*/
+            }
+
         }
         return returnString;
     }
