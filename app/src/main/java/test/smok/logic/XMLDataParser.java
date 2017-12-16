39c7e811ac5dbba9aeb43d8413fe68b09a5adb31
Kuba <jakubruszil@gmail.com>
2017-12-16 15:31:47 +0100

    Check commit

---
+++ app/src/main/java/test/smok/logic/XMLDataParser.java
@@ -0,0 +1,12 @@
+package test.smok.logic;
+
+/**
+ * Created by Kuba on 16.12.2017.
+ */
+
+public class XMLDataParser extends Parser {
+    @Override
+    public void parse(char delimiter1, char delimiter2) {
+
+    }
+}
