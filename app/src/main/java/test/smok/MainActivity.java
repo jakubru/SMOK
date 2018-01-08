324eeff2adf8ffcf02c8c83c64a01e9d3a98edf3
Kruszylo <titov@dlit.dp.ua>
2018-01-08 15:53:55 +0100

    perm proto

--- app/src/main/java/test/smok/MainActivity.java
+++ app/src/main/java/test/smok/MainActivity.java
@@ -1,30 +1,137 @@
 package test.smok;
 
-import android.content.Context;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.SparseIntArray;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import test.smok.logic.GSMDataCollector;
 import test.smok.logic.XMLDataParser;
 
-public class MainActivity extends AppCompatActivity {
-
-    public static Context context;
-
+public class MainActivity extends AppCompatActivity  implements ActivityCompat.OnRequestPermissionsResultCallback{
+    public static final int LOCATION_REQUEST_CODE = 0;
+    public static final int PHONE_REQUEST_CODE = 1;
+    private static final int REQUEST_PERMISSIONS = 124;
+    private SparseIntArray mErrorString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        MainActivity.context=getApplicationContext();
+        mErrorString = new SparseIntArray();
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        /*boolean allowed = true;
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted!
+                    // you may now do the action that requires this permission
+                    refreshClick();
+                } else {
+                    // we will give warning to user that they haven't granted permissions.
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                            Toast.makeText(this, "Storage Permissions denied.", Toast.LENGTH_SHORT).show();
 
+                        } else {
+                            showNoPermissionSnackbar();
+                        }
+                    }
+                }
+                break;
+            case PHONE_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted!
+                    // you may now do the action that requires this permission
+                    refreshClick();
+                } else {
+                    // we will give warning to user that they haven't granted permissions.
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                            Toast.makeText(this, "Storage Permissions denied.", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            showNoPermissionSnackbar();
+                        }
+                    }
+                }
+                break;
+                default:
+                    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }*/
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for (int permission : grantResults) {
+            permissionCheck = permissionCheck + permission;
+        }
+        if ((grantResults.length > 0) && permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            refreshClick();
+        } else {
+            Snackbar.make(findViewById(android.R.id.content), mErrorString.get(requestCode),
+                    Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent();
+                            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            intent.addCategory(Intent.CATEGORY_DEFAULT);
+                            intent.setData(Uri.parse("package:" + getPackageName()));
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                            startActivity(intent);
+                        }
+                    }).show();
+        }
+    }
+    public void requestAppPermissions(final String[] requestedPermissions,
+                                      final int stringId, final int requestCode) {
+        mErrorString.put(requestCode, stringId);
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        boolean shouldShowRequestPermissionRationale = false;
+        for (String permission : requestedPermissions) {
+            permissionCheck = permissionCheck + ContextCompat.checkSelfPermission(this, permission);
+            if(permissionCheck<=0){
+                int tmp = permissionCheck;
+            }
+            shouldShowRequestPermissionRationale = shouldShowRequestPermissionRationale || ActivityCompat.shouldShowRequestPermissionRationale(this, permission);
+        }
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale) {
+                Snackbar.make(findViewById(android.R.id.content), stringId,
+                        Snackbar.LENGTH_INDEFINITE).setAction("GRANT",
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ActivityCompat.requestPermissions(MainActivity.this, requestedPermissions, requestCode);
+                            }
+                        }).show();
+            } else {
+                ActivityCompat.requestPermissions(this, requestedPermissions, requestCode);
+            }
+        } else {
+            refreshClick();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -49,6 +156,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onRefreshButtonClick(View view){
+        requestAppPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_PHONE_STATE},
+                R.string.runtime_permissions_txt,
+                REQUEST_PERMISSIONS);
+    }
+    private void refreshClick(){
         GSMDataCollector g = new GSMDataCollector();
         TextView textView = (TextView) findViewById(R.id.SomeName);
         XMLDataParser xmlDataParser=new XMLDataParser(g);
